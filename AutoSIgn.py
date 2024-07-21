0e5426520140532d258c8f6c2dd9e61aa6fa455b
zhW-acn <132061556+zhw-acn@users.noreply.github.com>
2024-07-21 08:25:38 +0800

    init

---
+++ AutoSIgn.py
@@ -0,0 +1,114 @@
+# 打卡
+
+import json
+import time
+from datetime import datetime
+import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# 登陆的学号、密码和重试时间
+username = '320202010337'
+password = 'Wzhwzhwzh@123'
+retryTime = 1 * 60 * 60
+reLoginTime = 1 * 60
+signURL = 'http://ims.aiit.edu.cn/signMobile/saveSign.do'
+loginURL = 'https://in.aiit.edu.cn/uaac-server/login'
+
+
+def main():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(doCore, "cron", hour=12, minute=00)  # 每天中午12点打卡
+
+    try:
+        while True:
+            scheduler.start()
+            print("当前时间：", datetime.now())
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+
+
+# 登录
+def login():
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/126.0.0.0 Safari/537.36'
+    }
+
+    login_data = {
+        'username': username,
+        'password': password
+    }
+
+    login_session = requests.Session()
+
+    response = login_session.post(loginURL, headers=headers, data=login_data)
+
+    if response.status_code == 200:
+        print("登录成功")
+        return login_session
+    else:
+        print(f"登录失败，状态码: {response.status_code}")
+        print(f"错误信息: {response.text}")
+        return None
+
+
+# 获取响应JSON里的打卡排名信息
+def get_ranking(json_str):
+    data = json.loads(json_str)
+    ranking = data['data']['ranking']
+    return str(ranking)
+
+
+def doCore():
+    try:
+        core()
+    except ValueError:
+        time.sleep(retryTime)
+        core()
+
+
+# 打卡核心方法
+def core():
+    # 获取登录会话
+    session = None
+
+    while True:
+        try:
+            session = login()
+        except requests.exceptions.SSLError as e:
+            print("关闭代理")
+        if session is None:
+            print("登录失败，等待重试")
+            time.sleep(reLoginTime)
+            continue
+        break
+
+    # 设置要发送的表单数据
+    data = {
+        'access_token': '76c8bb52-f91b-47d1-bb2b-7bb1be0bc71d',
+        '_userCode': username
+    }
+
+    headers = {
+        # 'Content-Type': 'multipart/form-data; boundary=155e286c-6621-4875-a9ce-7170a730cba2',
+        # 'Content-Length': '1165',
+        # 'Host': 'ims.aiit.edu.cn',
+        # 'Connection': 'Keep-Alive',
+        # 'Accept-Encoding': 'gzip',
+        # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        #               'Chrome/126.0.0.0 Safari/537.36'
+    }
+
+    response = requests.post(signURL, headers=headers, data=data, verify=False)
+
+    if response.status_code == 200:
+        print("打卡成功，第" + get_ranking(response.text) + "名")
+    else:
+        print(f"打卡失败，状态码: {response.status_code}")
+        print(f"错误信息: {response.text}")
+        raise ValueError  # 随便抛一个异常
+
+
+if __name__ == '__main__':
+    main()
---
+++ DoubleWeekReport.py
@@ -0,0 +1,100 @@
+# 双周报
+import time
+
+import requests
+
+username = '320202010337'
+password = 'Wzhwzhwzh@123'
+retryTime = 1 * 60 * 60
+reLoginTime = 1 * 60
+signURL = 'http://ims.aiit.edu.cn/signMobile/saveSign.do'
+loginURL = 'https://in.aiit.edu.cn/uaac-server/login'
+reportURL = 'http://ims.aiit.edu.cn/weeklyMobile/save.do'
+
+
+# 登录
+def login():
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/126.0.0.0 Safari/537.36'
+    }
+
+    login_data = {
+        'username': username,
+        'password': password
+    }
+
+    login_session = requests.Session()
+
+    response = login_session.post(loginURL, headers=headers, data=login_data)
+
+    if response.status_code == 200:
+        print("登录成功")
+        return login_session
+    else:
+        print(f"登录失败，状态码: {response.status_code}")
+        print(f"错误信息: {response.text}")
+        return None
+
+
+# 打卡核心方法
+def core():
+    # 获取登录会话
+    session = None
+
+    while True:
+        try:
+            session = login()
+        except requests.exceptions.SSLError as e:
+            print("关闭代理")
+        if session is None:
+            print("登录失败，等待重试")
+            time.sleep(reLoginTime)
+            continue
+        break
+
+    headers = {
+        'Host': 'ims.aiit.edu.cn',
+        'Connection': 'keep-alive',
+        'Content-Length': '349',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Accept': 'application/json, text/plain, */*',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; SEA-AL10 Build/HUAWEISEA-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/046248 Mobile Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'http://ims.aiit.edu.cn',
+        'X-Requested-With': 'cn.edu.aiit.axx',
+        'Referer': 'http://ims.aiit.edu.cn/dist/index.html',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-JP;q=0.8,en-US;q=0.7,en;q=0.6'
+    }
+
+    data = {
+        'content': 'test本段时间工作内容1111111111111',
+        'completionAndExperience': 'test完成心得111111111111',
+        'startDate': '2024-07-07',
+        'endDate': '2024-07-20',
+        'whetherZc': 'false',
+        'docVo': '[]',
+        'userCode': '320202010337',
+        '_userCode': '320202010337',
+        'access_token': '76c8bb52-f91b-47d1-bb2b-7bb1be0bc71d',
+        '_userType': '1'
+    }
+
+    # 发送 POST 请求
+    response = requests.post(reportURL, headers=headers, data=data, verify=False)
+
+    # 检查响应状态码
+    if response.status_code == 200:
+        print("请求成功")
+        print("响应内容：", response.text)
+    else:
+        print(f"请求失败，状态码: {response.status_code}")
+        print(f"错误信息: {response.text}")
+        raise ValueError  # 随便抛一个异常
+
+
+if __name__ == '__main__':
+    core()
