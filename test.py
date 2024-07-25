adb592f3e56a7b553df51c9506ece7ea0adb9a89
zhW-acn <132061556+zhw-acn@users.noreply.github.com>
2024-07-25 21:56:50 +0800

    test Action ARGS

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
+import argparse
+
+
+# 命令行参数
+def parse_args():
+    parser = argparse.ArgumentParser(description="安小信实习打卡")
+    parser.add_argument('-u', required=True, help='学号')
+    parser.add_argument('-p', required=True, help='密码')
+    parser.add_argument('-hour', required=True, help='24进制小时')
+    parser.add_argument('-minute', required=True, help='分钟')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    print('test Action')
+    args = parse_args()
+    print(args)
